4adc568f4f08a3f8fe9a0fdc19a03d75a91c3f65
harshita-gupta <gupta.harshita.001@gmail.com>
2015-09-12 18:34:12 -0400

    Dummy internet connection established

--- Huzzah1/Huzzah1.ino
+++ Huzzah1/Huzzah1.ino
@@ -5,6 +5,13 @@ Then connect one end of a 10K resistor from Analog 0 to ground
  
 For more information see www.ladyada.net/learn/sensors/fsr.html */
 
+#include <ESP8266WiFi.h>
+ 
+const char* ssid     = "MHacks";
+const char* password = "Walmart!";
+ 
+const char* host = "www.adafruit.com";
+
 int numberOfVibrators = 1;
 int vibratePins[] = {2};
  
@@ -18,13 +25,63 @@ void setup(void) {
     pinMode(vibratePins[i], OUTPUT);
   }
 
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
+
  
 void loop(void) {
+
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/testwifi/index.html";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+
+
+
+
 
   for (int i = 0; i < numberOfVibrators ; i++) {
     digitalWrite(vibratePins[i], HIGH);
   }
+
+
+
 
 //  digitalWrite(2, HIGH);
   analogReading = analogRead(A0);  
